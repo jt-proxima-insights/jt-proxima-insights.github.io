9b147f93d49e3dd8cf892290b4df9e3bf202ec25
Jonathan Esquibel <jesquibe@usc.edu>
2021-01-20 00:07:05 -0800

    first commit, simple looker embed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Look from './containers/Look.js'
+
+function App() {
+  return(
+    <div className="App">
+      <Look />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Look.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Look () {
+  return(
+    <div className="look">
+      <iframe
+        src="https://deepr.cloud.looker.com/embed/looks/9"
+        title="Deepr Insights"
+        height="1000px"
+        width="100%"
+        >
+      </iframe>
+    </div>
+  )
+}
+
+export default Look;
